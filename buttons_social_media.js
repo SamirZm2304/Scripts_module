21d8fc608cf3112dc13f3fd2707e9207665eca48
SamirZm2304 <samir.zurita@gmail.com>
2023-06-09 12:04:06 -0500

    change icon

--- buttons_social_media.js
+++ buttons_social_media.js
@@ -1,31 +1,33 @@
 const button_component = ({ img_url, img_alt, link_url }) => {
-    return `
+  return `
     <a class="social_networks_button" href="${link_url}" target="_blank">
         <img src="${img_url}" alt="${img_alt}">
     </a>
-        `
-}
+        `;
+};
 
 // const button_open = `<button`
 
-
-const buttons = [{
-    img_alt: 'facebook_icon',
-    img_url: 'https://raw.githubusercontent.com/SamirZm2304/Scripts_module/main/images/fb.png',
-    link_url: 'https://m.me/103926132361200'
-},
-{
-    img_alt: 'instagram_icon',
-    img_url: 'https://raw.githubusercontent.com/SamirZm2304/Scripts_module/main/images/ins.png',
-    link_url: 'https://ig.me/m/cooperativaluchacampesina'
-
-},
-{
-    img_alt: 'WhatsApp_icon',
-    img_url: 'https://raw.githubusercontent.com/SamirZm2304/Scripts_module/main/images/wp.png',
-    link_url: 'https://wa.me/593990950119'
-
-}]
+const buttons = [
+  {
+    img_alt: "facebook_icon",
+    img_url:
+      "https://raw.githubusercontent.com/SamirZm2304/Scripts_module/main/images/fb.png",
+    link_url: "https://m.me/103926132361200",
+  },
+  {
+    img_alt: "instagram_icon",
+    img_url:
+      "https://raw.githubusercontent.com/SamirZm2304/Scripts_module/main/images/ins.png",
+    link_url: "https://ig.me/m/cooperativaluchacampesina",
+  },
+  {
+    img_alt: "WhatsApp_icon",
+    img_url:
+      "https://raw.githubusercontent.com/SamirZm2304/Scripts_module/main/images/wp.png",
+    link_url: "https://wa.me/593990950119",
+  },
+];
 
 const DEFAULT_STYLES = `
 #button_chat_social_networks {
@@ -84,26 +86,30 @@ const DEFAULT_STYLES = `
 }
 
 #social_networks_buttons .social_networks_button img {
-    width: 60px;
-    height: 60px;
+    width: 50px;
+    height: 50px;
     object-fit: contain;
 }
-`
+`;
 
-const body = document.querySelector('body');
-const button_open = document.createElement('button');
-const buttons_container = document.createElement('section');
-const styles_container = document.createElement('style');
+const body = document.querySelector("body");
+const button_open = document.createElement("button");
+const buttons_container = document.createElement("section");
+const styles_container = document.createElement("style");
 
-buttons_container.setAttribute('id', 'social_networks_buttons');
-button_open.setAttribute('id', 'button_chat_social_networks');
+buttons_container.setAttribute("id", "social_networks_buttons");
+button_open.setAttribute("id", "button_chat_social_networks");
 button_open.innerHTML = `
-<img src="https://raw.githubusercontent.com/R00rss/scripts_modulos/main/images/chat.png" alt="open_buttons_chat">
+<img src="https://raw.githubusercontent.com/SamirZm2304/Scripts_module/main/images/chat.png" alt="open_buttons_chat">
     <p>chatea con nosotros</p> 
 </img>
 
 `;
-buttons_container.innerHTML = buttons.map(button => { return button_component(button) }).join('')
+buttons_container.innerHTML = buttons
+  .map((button) => {
+    return button_component(button);
+  })
+  .join("");
 styles_container.innerHTML = DEFAULT_STYLES;
 
 body.appendChild(button_open);
